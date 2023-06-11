c14ae5edc533d4eb52684dc720e2f9818ad97475
salwatasya <salwatasyafthr@gmail.com>
2023-06-11 21:03:57 +0700

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { getUsers, register, Login, Logout } from "../controller/users.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { refreshToken } from "../controller/refreshToken.js";
+
+const router = express.Router();
+
+router.get('/users', verifyToken, getUsers);
+// post blm berjalan
+router.post('/users', register);
+router.post('/login', Login);
+router.get('/token', refreshToken);
+router.delete('/logout', Logout);
+
+export default router;
